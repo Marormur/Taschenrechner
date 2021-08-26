a6254c1924dae97a103fe95e90260d73196c84e9
Marormur <marvintemmen@gmail.com>
2021-08-26 10:30:27 +0200

    Einfaches Handling von Tastatureingaben

--- Taschenrechner/MainWindowViewModel.cs
+++ Taschenrechner/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Linq;
 using Microsoft.Xaml.Behaviors.Core;
 using Taschenrechner.Annotations;
 
@@ -10,8 +11,16 @@ namespace Taschenrechner
 {
     class MainWindowViewModel : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Komplette Rechenformel
+        /// </summary>
+        private string kompletteRechnung;
+
         private string eingabe = "0";
 
+        /// <summary>
+        /// Die Eingabe, welche dem Nutzer angezeigt wird.
+        /// </summary>
         public string Eingabe
         {
             get => eingabe;
@@ -27,17 +36,57 @@ namespace Taschenrechner
             }
         }
 
+        private readonly char[] operatoren = new char[] { '+', '-', '*', '/','=' };
+
         /// <summary>
         /// Wenn einer der Hauptbuttons betätigt wurde.
         /// </summary>
         public ICommand GenericButtonClickCommand => new ActionCommand(delegate(object o)
         {
-            if (o is not RoutedEventArgs {Source: Button knopf})
+            if (o is not RoutedEventArgs {Source: Button knopf })
             {
                 return;
             }
 
-            eingabe += knopf.Content.ToString();
+            this.AddInput(knopf.Content.ToString() ?? string.Empty);
+        });
+
+        private void AddInput(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return;
+            }
+
+            if (this.operatoren.Any(op => op.Equals(input[0])))
+            {
+                this.Eingabe = string.Empty;
+            } 
+            else
+            {
+                this.Eingabe += input;
+            }
+
+            this.kompletteRechnung += input;
+        }
+
+        /// <summary>
+        /// Wenn das Fenster Tastatureingaben empfängt.
+        /// </summary>
+        public ICommand HardwareKeyUpCommand => new ActionCommand(delegate (object o)
+        {
+            if (o is not KeyEventArgs args)
+            {
+                return;
+            }
+
+            string keyString = new KeyConverter().ConvertToString(args.Key);
+            if (keyString.Length > 1 || !char.IsDigit(keyString[0]) || !this.operatoren.Any(op => op.Equals(keyString[0])))
+            {
+                return;
+            }
+
+            this.AddInput(keyString);
         });
 
         public event PropertyChangedEventHandler? PropertyChanged;
---
+++ Taschenrechner/Rechner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    /// <summary>
+    /// Diese Klasse rechnet.
+    /// </summary>
+    internal class Rechner
+    {
+
+    }
+}
